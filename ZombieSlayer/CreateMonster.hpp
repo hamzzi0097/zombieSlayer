040fa1fc90c81879e04920c94dda8fec6e8bb208
nouhyeon <nou2325@naver.com>
2026-05-21 00:08:45 +0900

    근거리, 원거리 , 몬스터 생성 클래스 정의

--- ZombieSlayer/CreateMonster.hpp
+++ ZombieSlayer/CreateMonster.hpp
@@ -1,20 +1,31 @@
 #pragma once
 #include "Framework.hpp"
 
+#define WIDTH 430
+#define LENGTH 330
+
 class CreateMonster {
 private:
   std::pair<int, int> left;
   std::pair<int, int> right;
   std::pair<int, int> down;
   std::pair<int, int> up;
-
+  std::random_device rd;
+  std::mt19937 gen;
+  std::uniform_int_distribution<int> distrib;
 public:
 
-  CreateMonster() {
-    left = { -1,0 };
-    right = { 1,0 };
-    up = { 0,1 };
-    down = { 0,-1 };
-  }
 
+  CreateMonster() :left({ -WIDTH,0 }),    right({WIDTH, 0}),    up({ 0,LENGTH }),    down  ({ 0,-LENGTH }), gen(rd()), distrib(1, 800){
+
+  }
+  void generationMonster() {
+    int positonX=distrib(gen)-400;
+    int positonY = distrib(gen)%600 - 300;
+    left.second = positonY;
+    right.second = positonY;
+    up.first = positonX;
+    down.first = positonX;
+    // 몬스터 생성되는 위치만 네 방향으로 설정, 여기서 하나 불러서 사용하면 될 듯 함
+  }
 };
--- ZombieSlayer/MeleeMonster.hpp
+++ ZombieSlayer/MeleeMonster.hpp
@@ -20,14 +20,21 @@ public:
     switch (meleeState)
     {
     case state::TRACE:
-
+      //플레이어한테 이동
+      //범위체크=> 여기서 공격할 지, 죽을 지 정함
       break;
     case state::DEAD:
-
+      delete this;
       break;
     case state::ATTACK:
-
+      // 근거리라 일단 보류
       break;
     }
   }
+
+  void ChangeState(state nextState) {
+    meleeState = nextState;
+  }
+
+
 };
